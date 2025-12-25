966c5e5234af1f2f8ec4760d62330790e9046bcd
Omar <omar-aljalam@outlook.com>
2025-12-26 00:22:26 +0200

    Adding the Main Modeles

--- machine/models.py
+++ machine/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class MachineType(models.Model):
+    name = models.CharField(max_length=50)
+    maintenance_interval_hours = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Machine(models.Model):
+    name = models.CharField(max_length=50)
+    machine_type = models.ForeignKey("MachineType", on_delete=models.CASCADE, related_name="machines")
+    total_operating_hours = models.PositiveIntegerField()
+    last_service_hours = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
