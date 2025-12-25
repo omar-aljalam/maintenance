4b26ec09e0954a8e07318f8f38f8d04dcb0156b3
Omar <omar-aljalam@outlook.com>
2025-12-26 01:43:44 +0200

    Configure admin page and add some data

--- machine/admin.py
+++ machine/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from .models import Machine, MachineType
+
 # Register your models here.
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "machine_type",
+        "total_operating_hours",
+        "last_service_hours",
+        "status",
+        "hours_remaining",
+        "overdue_hours",
+    )
+
+    def status(self, obj):
+        return obj.get_status()
+
+    list_filter = (
+        "machine_type",
+    )
+
+@admin.register(MachineType)
+class MachineTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "maintenance_interval_hours",
+    )
